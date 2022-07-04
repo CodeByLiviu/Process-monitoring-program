60fa5ad11efa71f5faa2a9f41b7160dd88e69cdb
Liviu Moga <107944475+CodeByLiviu@users.noreply.github.com>
2022-07-04 09:10:05 +0300

    Update and rename processMonitor.py to process-monitoring-program.py

--- process-monitoring-program.py
+++ process-monitoring-program.py
@@ -13,21 +13,24 @@ Collected data should be stored on the disk. Format of stored data should suppor
 '''
                                           READ THIS
                         ***   Steps to fallow to use this program   ***
-1. Choose the folder where the excel file will be stored!
-2. Create an excel file in the chosen folder and name it, the excel file must end with the extension .elsx, for example: RaportStatus.elsx.
+before you hit the start button you need a excel file to store all informations.                        
+1. Choose the folder where the excel file will be!
+2. Create an excel file inside the chosen folder and name it, the excel file must end with the extension .elsx, for example: RaportStatus.elsx.
 3. Open the excel file and write what is in model: 9 cells, this is the first and only line we wrote horizontally (row arranged on horizontal). 
    A row arranges data horizontally from left to right. 
    model: 1. Time,	2. PID,	3. Process CPU utilization (%),	4. Process Memory utilization,	5. Process Memory utilization(%),	6. Number of open handles,	7. Number of open threads,	8. Memory usage on the whole system (%),	9. Cpu usage on the whole system (%);
-4. Save the excel file, exit, and copy the path to the excel file!
-5. Choose the interval(seconds) between updates, it must be a integer number!
+4. Save the excel file, exit, and copy the path to the excel file! exemple: path = 'C:/Users/Liviu/Desktop/data/StatusReport.xlsx'
+5. Start the program 
+6. Choose the interval(seconds) between updates, it must be a integer number!
 6. Choose the PID, you can find it in taskmanager (windows system), in section "Details", PID must be an integer!
-6. Start the program enter and enter these values: interval = 5, pid = 6060, path = C:/Users/Liviu/Desktop/data/StatusReport.xlsx 
+full exemple: interval = 5, pid = 6060, path = 'C:/Users/Liviu/Desktop/data/StatusReport.xlsx' 
 THIS PROGRAM STORES THE DESIRED DATA FOR A PROCESS THAT IS ALREADY RUNNING, OR WILL BE STARTED, YOU HAVE TO GIVE IT THE PID(MANUALLY) AND PUT IT IN THIS PROGRAM TO SEE THE PROCESS STATISTICS!
                               THIS PROGRAM DOES NOT START A DESIRED PROCESS!
 '''
 # when you will use the program make sure the excel file is closed.
 # if it's not: you will get some errors!
-# you can't use the program while excel file is open!
+# you can't use the program while excel file is open,
+# while the program is running don't open excel file!
 # some PIDs can't be used, because they have some windows protection.
 # Example of PIDs with protection: SYSTEM, LOCAL SERVICE, NETWORK SERVICE, DWM-2, UMFD-0...
 # If you want to try one of this pids with protection, you will get an error "Acces denied"!
